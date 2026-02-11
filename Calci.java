d63e75af94ae0df525b01c419b41a6ee4dba5a89
Manasvee Chavan <chavanmanasvee@gmail.com>
2026-02-11 15:58:43 +0530

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -20,31 +20,27 @@ public class Calci {
         System.out.print("Enter second number: ");
         double num2 = sc.nextDouble();
 
-        switch (choice) {
-            case 1:
-                System.out.println("Result: " + (num1 + num2));
-                break;
-
-            case 2:
-                System.out.println("Result: " + (num1 - num2));
-                break;
-
-            case 3:
-                System.out.println("Result: " + (num1 * num2));
-                break;
-
-            case 4:
-                if (num2 != 0) {
-                    System.out.println("Result: " + (num1 / num2));
-                } else {
-                    System.out.println("Error! Division by zero is not allowed.");
-                }
-                break;
-
-            default:
-                System.out.println("Invalid choice!");
+        if (choice == 1) {
+            System.out.println("Result: " + (num1 + num2));
+        }
+        else if (choice == 2) {
+            System.out.println("Result: " + (num1 - num2));
+        }
+        else if (choice == 3) {
+            System.out.println("Result: " + (num1 * num2));
+        }
+        else if (choice == 4) {
+            if (num2 != 0) {
+                System.out.println("Result: " + (num1 / num2));
+            } else {
+                System.out.println("Error! Division by zero is not allowed.");
+            }
+        }
+        else {
+            System.out.println("Invalid choice!");
         }
 
         sc.close();
     }
 }
+
